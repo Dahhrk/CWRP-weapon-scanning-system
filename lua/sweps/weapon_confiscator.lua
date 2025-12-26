06e7e7a637cb0450635da23188135c320f7da80f
Dark <32241100+Dahhrk@users.noreply.github.com>
2025-12-26 16:10:51 +0000

    Create weapon_confiscator.lua

---
+++ lua/sweps/weapon_confiscator.lua
@@ -0,0 +1,28 @@
+-- Weapon Confiscator SWEP for Clone Wars Roleplay
+
+AddCSLuaFile()
+
+SWEP.PrintName = "Weapon Confiscator"
+SWEP.Author = "Dahhrk"
+SWEP.Spawnable = true
+SWEP.AdminOnly = true
+
+function SWEP:PrimaryAttack()
+    if CLIENT then return end
+
+    local ply = self:GetOwner()
+    local target = ply:GetEyeTrace().Entity
+
+    if not IsValid(target) or not target:IsPlayer() then
+        ply:ChatPrint("No player detected. Aim at a player to confiscate weapons.")
+        return
+    end
+
+    local confiscatedWeapons = {}
+    for _, weapon in ipairs(target:GetWeapons()) do
+        table.insert(confiscatedWeapons, weapon:GetClass())
+        target:StripWeapon(weapon:GetClass())
+    end
+
+    hook.Run("CWRP_PlayerConfiscated", ply, target, confiscatedWeapons)
+end
